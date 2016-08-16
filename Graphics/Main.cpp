40ced8baa1371e02513be01bb55878bf1fd0ecc6
Eugene Hochenedel <eugeneatschool@gmail.com>
2016-08-16 18:07:44 -0500

    Gizmos Implemented
    Implemented gizmos into the tutorial practicel.

--- Graphics/Main.cpp
+++ Graphics/Main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include "src\gl_core_4_4.h"
 #include <GLFW\glfw3.h>
+#include "src\Gizmos.h"
+#include <glm.hpp>
+#include <ext.hpp>
+
+using glm::vec3;
+using glm::vec4;
+using glm::mat4;
 
 int main()
 {
@@ -26,9 +33,42 @@ int main()
 		return -3;
 	}
 
+	Gizmos::create();
+
+	mat4 view = glm::lookAt(vec3(10, 10, 10), vec3(0), vec3(0, 1, 0));
+	mat4 projection = glm::perspective(glm::pi<float>() * 0.25f, 16 / 9.f, 0.1f, 1000.f);
+
 	auto major = ogl_GetMajorVersion();
 	auto minor = ogl_GetMinorVersion();
 	printf("GL: %i.%i\n", major, minor);
+
+	glClearColor(0.25f, 0.25f, 0.25f, 1);
+	glEnable(GL_DEPTH_TEST);
+
+	while (glfwWindowShouldClose(window) == false && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS)
+	{
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		Gizmos::clear;
+
+		//Gizmos::addTransform(glm::mat4(1));
+
+		vec4 white(1);
+		vec4 black(0, 0, 0, 1);
+
+		for (int i = 0; i < 21; i += 1)
+		{
+			Gizmos::addLine(vec3(-10 + i, 0, 10), vec3(-10 + i, 0, -10), i == 10 ? white : black);
+			Gizmos::addLine(vec3(10, 0, -10 + i), vec3(-10, 0, -10 + i), i == 10 ? white : black);
+		}
+
+		Gizmos::draw(projection * view);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	Gizmos::destroy();
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
