3da11b260e9438471bf9b62bf92999c1fb777c44
Eugene Hochenedel <eugeneatschool@gmail.com>
2016-09-13 10:39:09 -0500

    Redo of Bindings
    Rewrote code for bindings in the generate grid function.
    No longer throws an exception at run time.

--- Rendering Geometry/RenderingGeometry.cpp
+++ Rendering Geometry/RenderingGeometry.cpp
@@ -29,73 +29,51 @@ Renderer::Renderer()
 //Function to create a grid
 void Renderer::generateGrid(unsigned int rows, unsigned int columns)
 {
-	Vertex* aoVertices = new Vertex[rows * columns];
-	for (unsigned int i = 0; i < rows; i++)
-	{
-		for (unsigned int j = 0; j < columns; j++)
-		{
-			aoVertices[i * columns + j].position = glm::vec4((float)j, 0, (float)i, 1);
+	Vertex aoPoints[4];
+	unsigned int auiIndex[4] = { 0, 1, 2, 3 };
 
-			//Create some arbitrary color based off something that might not be related to tiling a texture
-			glm::vec3 colour = glm::vec3(sinf((j / (float)(columns - 1)) * (i / (float)(rows - 1))));
-			aoVertices[i * columns + j].colour = glm::vec4(colour, 1);
-		}
-	}
+	aoPoints[0].position = glm::vec4(2, 0, 4, 0);
+	aoPoints[1].position = glm::vec4(2, 0, -4, 0);
+	aoPoints[2].position = glm::vec4(-4, 0, -4, 0);
+	aoPoints[3].position = glm::vec4(-4, 0, 4, 0);
 	
-	//Defines index count based off quad count (2 triangles per quad)
-	//Creates an index array as the grid is being drawn
-	unsigned int* auiIndices = new unsigned int[(rows - 1) * (columns - 1) * 6];
-
-	unsigned int index = 0;
-	for (unsigned int i = 0; i < (rows - 1); i++)
-	{
-		for (unsigned int j = 0; j < (columns - 1); j++)
-		{
-			//Triangle 1
-			auiIndices[index++] = i * columns + j;
-			auiIndices[index++] = (i + 1) * columns + j;
-			auiIndices[index++] = (i + 1) * columns + (j + 1);
-
-			//Triangle 2
-			auiIndices[index++] = i * columns + j;
-			auiIndices[index++] = (i + 1) * columns + (j + 1);
-			auiIndices[index++] = i * columns + (i + 1);
-		}
-	}
+	aoPoints[0].colour = glm::vec4(1, 0, 0, 1);
+	aoPoints[1].colour = glm::vec4(0, 1, 0, 1);
+	aoPoints[2].colour = glm::vec4(0, 0, 1, 1);
+	aoPoints[3].colour = glm::vec4(1, 1, 1, 1);
 
 	//Generates a vertex buffer object (VBO)
 	glGenBuffers(1, &m_VBO);
+	
 	//Generates an index buffer object (IBO)
 	glGenBuffers(1, &m_IBO);
-
+	
 	//Generates a vertex array object (VAO)
 	glGenVertexArrays(1, &m_VAO);
-
+	
+	//Binds the VAO
 	glBindVertexArray(m_VAO);
 
 	//Binds and fills the VBO
 	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-	glBufferData(GL_ARRAY_BUFFER, (rows * columns) * sizeof(Vertex), aoVertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 4 * sizeof(Vertex), aoPoints, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
-	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(glm::vec4)));
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	//Binds and fills the IBO
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, (rows - 1) * (columns - 1) * 6 * sizeof(unsigned int), auiIndices, GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-	//Binds and fills the VAO
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 4 * sizeof(unsigned int), auiIndex, GL_STATIC_DRAW);
+	
+	//Binds the arrays
 	glBindVertexArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
-	delete[] aoVertices, auiIndices;
+	//delete[] aoVertices, auiIndices;
 }
 
 bool Renderer::startup()
@@ -170,13 +148,14 @@ void Renderer::draw()
 	generateGrid(rows, columns);
 	m_projectionViewMatrix = projection * view;
 
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 	glUseProgram(m_programID);
 	unsigned int projectionViewUniform = glGetUniformLocation(m_programID, "projectionViewWorldMatrix");
 	glUniformMatrix4fv(projectionViewUniform, 1, false, glm::value_ptr(m_projectionViewMatrix));
 
 	glBindVertexArray(m_VAO);
 	indexCounter = (rows - 1) * (columns - 1) * 6;
-	//Exception thrown when passing in GL_UNSIGNED_INT
 	glDrawElements(GL_TRIANGLES, indexCounter, GL_UNSIGNED_INT, 0);
 
 	glfwSwapBuffers(screen);
