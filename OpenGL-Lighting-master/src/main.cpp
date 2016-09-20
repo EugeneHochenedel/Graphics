43ec66506bdfb27b6e7d77580d10e316d4954693
Eugene Hochenedel <eugeneatschool@gmail.com>
2016-09-20 11:58:03 -0500

    Initial Commit
    Worked with the fong vertex and fragment shaders.
    Made text copies of my progress with the shaders.

---
+++ OpenGL-Lighting-master/src/main.cpp
@@ -0,0 +1,15 @@
+ 
+#include "LightingOBJApplication.h"
+#include "LightingSphereApplication.h" 
+
+int main() {
+	
+	// change startup application
+	BaseApplication* app = new LightingSphereApplication();
+	if (app->startup())
+		app->run();
+	app->shutdown();
+
+	system("pause");
+	return 0;
+}
